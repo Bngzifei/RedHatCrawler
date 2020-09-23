e95da3911545037049d032b16af379e71d56b222
Bngzifei <38546885+Bngzifei@users.noreply.github.com>
2020-09-23 17:55:30 +0800

    [FIX]更新说明文档

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RedHatCrawler
 Data crawling RedHat's official website
+
+本项目是爬取RedHat网站的rhel7 和rhel8的changelog，并按照日期及changelog中的类别分别存储。
+
+爬取结果如下：
+
+![](img/img-ret01.png)
+
+未经处理的日志内容和官方网站的一致。
+
+执行 analyse_data.py 脚本，将上述changelog数据按照更新日期、类别分别存储，结果如下：
+
+![](img/img-ret02.png)
+
+按照日期分别存储:
+
+![](img/img-ret03.png)
--- analyse_data/analyse_data.py
+++ analyse_data/analyse_data.py
@@ -3,13 +3,15 @@ import re
 import os
 
 SRC_FILE_PATH = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel8_changelog/"
-SRC_FILE_PATH7 = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel7_changelog/"
+# SRC_FILE_PATH7 = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel7_changelog/"
+SRC_FILE_PATH7 = r"/home/xyc/LB/rhel8/"
 
 DATE_PATTERN = r"([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))"
 CATEGORY_PATTERN = r"- (\[)\w+(\])"
 
 # DST_FILE_PATH_PREFIX = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel8/"
-DST_FILE_PATH_PREFIX = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel7/"
+# DST_FILE_PATH_PREFIX = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel7/"
+DST_FILE_PATH_PREFIX = r"/home/xyc/LB/rhel8_change/"
 
 
 # DST_CATEGORIES_DIRS = r"/sf/data/local/TESTCASE/HW_Platform/test_script/rhel8/"
@@ -166,7 +168,6 @@ class RHELChangeLogHandler:
                     all_log_file_datas[filename] = title_objs
 
         return all_log_file_datas
-
 
     # make_changelog_file_by_rhel_txt(SRC_FILE_PATH)
     def get_all_files_name(self):
